2db0cad0e81811b4762322a1ad703d80084b2882
AnilKumarVijayagiri <121851145+AnilKumarVijayagiri@users.noreply.github.com>
2025-08-28 01:59:30 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ export default function Navbar(){
     <header className="sticky top-0 z-50 bg-white/90 backdrop-blur border-b">
       <div className="max-w-6xl mx-auto px-4 py-3 flex items-center justify-between">
         <Link to="/" className="flex items-center gap-2">
-          <img src="https://instagram.fhyd2-3.fna.fbcdn.net/v/t51.2885-19/530618089_17847919392538885_2041862342983925010_n.jpg?stp=dst-jpg_s320x320_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLmRqYW5nby45NjguYzIifQ&_nc_ht=instagram.fhyd2-3.fna.fbcdn.net&_nc_cat=101&_nc_oc=Q6cZ2QENQBd6g78l506WvV9iPEdSuVgQ1px7poXITPVoD6KPRtPSb-UcMMZ-00z-jWuPyyP2CwvhbtNWPO7CHubyY7zQ&_nc_ohc=j7ZIGVbZVS4Q7kNvwE0Zdu7&_nc_gid=xZFMtg8HsthMEUnMOFn7KQ&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfVFDgY4ut_NOdEczw0ZtsShEsSggN3tI6VIpp88GX6R-w&oe=68B52906&_nc_sid=8b3546" alt="logo" className="w-7 h-7"/>
+          <img src="https://ibb.co/nNwPLNGn" alt="logo" className="w-7 h-7"/>
           <span className="text-2xl font-extrabold text-brand-700">Shaivyah</span>
         </Link>
         <nav className="flex items-center gap-6">
